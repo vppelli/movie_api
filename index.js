db5f8c10cf130df524ff90f1173e8fda90b0965f
Mikey P <138417934+vppelli@users.noreply.github.com>
2023-12-08 17:10:47 -0500

    Updated index.js
    Updated Cors

--- index.js
+++ index.js
@@ -27,7 +27,19 @@ app.use(express.urlencoded({ extended: true }));
 
 const cors = require('cors');
 
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+//allow specific set of origins to access your API
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            // If a specific origin isn’t found on the list of allowed origins
+            let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 let auth = require('./auth')(app);
 
