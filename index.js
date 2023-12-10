74efd8e9b7bf7e029e7dd5430658572ef700fc03
Mikey P <138417934+vppelli@users.noreply.github.com>
2023-12-10 13:40:53 -0500

    Revert "Updated index.js"
    This reverts commit e4b5a8f977f7aa8ba2b0c08ed97ce80f1a406d0f.

--- index.js
+++ index.js
@@ -35,9 +35,6 @@ const passport = require('passport');
 
 require('./passport');
 
-// saves login on app after refreshing page
-app.use(session({ secret: 'keyboard cat', resave: false }))
-
 // Setup static serve
 app.use(express.static('public'));
 
