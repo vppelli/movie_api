3452c1e09a0967de1164f20b622aa4981054835b
Mikey P <138417934+vppelli@users.noreply.github.com>
2023-12-18 12:17:38 -0500

    Updated index.js
    Added Cors allowedOrigins list

--- index.js
+++ index.js
@@ -27,7 +27,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //allow specific set of origins to access your API
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://moviemikes.netlify.app'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require('./auth')(app);
 
